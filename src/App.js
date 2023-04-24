f0d51ae083a963f8a653d2025496c825453d9337
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-04-24 11:37:28 -0400

    Adjust basic CSS/HTML and add first Route to connect to db

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import { Switch, Route} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import ReservationList from "./components/ReservationList";
+import ReservationForm from "./components/ReservationForm"
+import Vehicles from "./components/Vehicles"
 
 function App() {
+  const [viewRentals, setViewRentals] = useState([])
+
+useEffect(()=>{
+        fetch('http://localhost:9292/reservations')
+        .then(res => res.json())
+        .then(data=> console.log(setViewRentals(data)) )
+    },[])
+
+  //   function handleNewEntry(newQuestion){
+  //     setTriviaQuestions([...triviaQuestions, newQuestion])
+  // }
+
+  // function deleteQuestion(deletedQuestion){
+  //   const filteredQuestionList = triviaQuestions.filter(trivia => trivia.id !== deletedQuestion.id)
+  //     return setTriviaQuestions(filteredQuestionList) 
+  //  }
+
+
+  // JSX to be returned
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* <header className="App-header">
+        <NavBar />
       </header>
+      <Switch> 
+        <Route path="/"> 
+        </Route>
+        <Route path="/reserved"> */}
+          <ReservationList viewRentals={viewRentals} />  
+       {/* </Route>
+        <Route exact path="/vehicles"> 
+          <Vehicles />
+        </Route> 
+        <Route path="/form">  
+          <ReservationForm  /> 
+        </Route> 
+      </Switch>         */}
     </div>
   );
 }
