bda98aa971ff5072f078f46afb4483bacac381dc
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-05-08 22:20:02 -0400

    add feature to show/hide reserved vehicle

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,15 +1,26 @@
 import React , {useState} from "react"
 // import Reservation from "./Reservation";
 
-function Vehicles({vehicle}){
+function Vehicles({vehicle,reservations}){
   const [showReservation, setShowReservation] = useState(false)
   
-  
+   const reserve = reservations.map(res =>  res.vehicle_id ? "Yes" : "No")
+
   function showReserved(){
     setShowReservation(!showReservation)
-    
   }
 
+   const reserved = reservations.map(res=> {
+        return( 
+        <div style={{padding: "15px"}} key={res.id}>
+          <b>Full Name:</b> {res.full_name} <br/>   
+          <b>Driver's License:</b> {res.driving_license} <br/>  
+          <b>Payment Method:</b> {res.payment_method }<br/>  
+        </div> 
+        )
+  })
+    
+  
 
   
 return(
@@ -19,11 +30,21 @@ return(
         <b>Car Type:</b> {vehicle.car_type} <br/>
         <b>Make and Model: </b>{vehicle.make_and_model}<br/>
         <b>License Plate:</b> {vehicle.license_plate}<br/>
-        <b>Reserved:</b> {vehicle.reserved} <br/>
+        <b>Reserved:</b> {reserve} <br/> 
         <hr/>
-          <button onClick={showReserved} value={vehicle.reservation}> Reservation Info</button> <br/>
+          <button onClick={showReserved} value={vehicle.id}> Reservation Info</button> <br/>
         <div style={{padding: "15px"}}> 
-          {showReservation ? <div style={{padding: "15px"}}> {vehicle.reservation} </div>  : null}
+          {showReservation ? reservations.map(res=> {
+                  return( 
+                  <div style={{padding: "15px"}} key={res.id}>
+                    <b>Full Name:</b> {res.full_name} <br/>   
+                    <b>Driver's License:</b> {res.driving_license} <br/>  
+                    <b>Payment Method:</b> {res.payment_method }<br/>  
+                  </div> 
+                  )
+                  }) 
+            : null
+          } 
         </div>
         
         {/* If car is reserved OnClick you will be able to see the reservation */}
