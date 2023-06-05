37f468394c9e6ad634ddd9d454787d509523e3ce
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-06-05 10:32:27 -0400

    remove vehicle ID being displayed

--- src/components/Vehicle.js
+++ src/components/Vehicle.js
@@ -11,7 +11,6 @@ function Vehicle({vehicle, onDelete, onItemUpdate}){
   
 return(
     <div>
-        <b>Vehicle ID:</b> {vehicle.id} <br/>
         <b>VIN:</b> {vehicle.vin} <br/>
         <b>Car Type:</b> {vehicle.car_type} <br/>
         <b>Make and Model: </b>{vehicle.make_and_model}<br/>
@@ -21,7 +20,7 @@ return(
           <button onClick={showReserved} value={vehicle.id}> Reservation Info</button> <br/>
         <div style={{padding: "15px"}}> 
           {showReservation ? (vehicle.reservations).map(reservation => 
-            <div  key={reservation.id}>
+            <div  key={reservation.id}  style={{marginTop:"40px"}}>
               <Reservation  reservation={reservation}
                             onDelete={onDelete} 
                             onItemUpdate={onItemUpdate} 
